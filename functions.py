80b2ed92679be11ba703925901d9b7a62466a2a7
Eugene Fedorov <uptiik02@gmail.com>
2024-12-18 23:32:02 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,23 +1,31 @@
-def fix_marks(schoolkid):
-    marks = Mark.objects.filter(schoolkid=schoolkid, points__in=[1, 2, 3])
-    for mark in marks:
-        mark.points = 5
-        mark.save()
+def fix_marks(name):
+    schoolkid = get_schoolkid(name)
+    marks = Mark.objects.filter(schoolkid=schoolkid, points__in=[1, 2, 3]).update(points=5)
     return None
 
 
-def remove_chastisements(schoolkid):
-    schoolkid_chastisements = Chastisement.objects.filter(schoolkid=schoolkid)
-    for chastisement in schoolkid_chastisements:
-        chastisement.delete()
+def remove_chastisements(name):
+    schoolkid = get_schoolkid(name)
+    chastisements = Chastisement.objects.filter(schoolkid=schoolkid).delete()
     return None
 
 
 def create_commendation(name, subject):
-    schoolkid = Schoolkid.objects.get(full_name__contains=name)
-    schoolkid_subject = Subject.objects.get(title=subject, year_of_study=schoolkid.year_of_study)
+    schoolkid = get_schoolkid(name)
+    schoolkid_subject = Subject.objects.get_object_or_404(title=subject, year_of_study=schoolkid.year_of_study)
     lessons = Lesson.objects.filter(year_of_study=schoolkid.year_of_study, group_letter=schoolkid.group_letter, subject=schoolkid_subject)
-    last_lesson = lessons.order_by('date').reverse()[0]
+    last_lesson = lessons.order_by('date').last()
+    if last_lesson:
+        last_lesson = lessons.order_by('date').last()
+    else:
+        print('Урок не найден!')
     Commendation.objects.create(text='Молодец!', created=last_lesson.date, schoolkid=schoolkid, subject=schoolkid_subject, teacher=last_lesson.teacher)
     return None
 
+
+def get_schoolkid(name):
+    try:
+        schoolkid = Schoolkid.objects.get(full_name__contains=name)
+    except Schoolkid.MultipleObjectsReturned:
+        print('Уточните введённые данные')
+    return schoolkid
